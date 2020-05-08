50cac2e7f0d505f894453a79254d6de432d7294f
aishwary023 <aishwary.saxena.min19@itbhu.ac.in>
2020-05-09 00:01:09 +0530

    Updates

---
+++ precache-manifest.a3ac5c5f5cb1578ec2464129f029ea21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad95a4038c717789582228a72c9ef19",
+    "url": "/SQLFormatter/index.html"
+  },
+  {
+    "revision": "20072574e3a1fedca3ad",
+    "url": "/SQLFormatter/static/css/main.6eabc335.chunk.css"
+  },
+  {
+    "revision": "6c1ad0b289d3d719e355",
+    "url": "/SQLFormatter/static/js/2.b757406c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SQLFormatter/static/js/2.b757406c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20072574e3a1fedca3ad",
+    "url": "/SQLFormatter/static/js/main.94e8e218.chunk.js"
+  },
+  {
+    "revision": "0c304749856d4da77fbb",
+    "url": "/SQLFormatter/static/js/runtime-main.2c50e643.js"
+  }
+]);
